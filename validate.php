034f6daa4428a1e30024c12d3dfc736f2fd014e5
Pablo Papalardo <papalardo94@hotmail.com>
2019-02-06 09:10:56 -0200

    Ajustes

--- validate.php
+++ validate.php
@@ -4,23 +4,19 @@ require 'vendor/autoload.php';
 
 use SMTPValidateEmail\Validator as SmtpEmailValidator;
 
-/**
- * Simple example
- */
 $email     = $_GET['email'];
 $sender    = 'no-reply@gmail.com';
 $validator = new SmtpEmailValidator($email, $sender);
 
 // $validator->setConnectTimeout(120);
 
-// If debug mode is turned on, logged data is printed as it happens:
 // $validator->debug = true;
+
 $results   = $validator->validate();
 echo json_encode($results);
 return;
 
-// var_dump($results);
+// See logs
 
-// Get log data (log data is always collected)
 // $log = $validator->getLog();
 // var_dump($log);
